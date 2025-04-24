b056952119bc451dde74c650055d69c02bc7d946
Robertron624 <robert.624@hotmail.com>
2025-04-23 22:29:15 -0500

    feat: adding functionality
    Now buttons do their functions, and the text is updated accordingly.

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,30 @@ import './style.css'
 
 
 function main(){
+  let savedConsent = localStorage.getItem('cookie-consent')
+  if(savedConsent === 'true') return
+    
+
+  let consent = false
+
   const cookiesWrapper = document.querySelector('.cookies-wrapper')
-
-  console.log(cookiesWrapper)
-
+  const closeButton = document.querySelector('.cookies-wrapper .close')
+  const acceptButton = document.querySelector('.cookies-wrapper .accept')
+  
   if(!cookiesWrapper) return
+
+  closeButton?.addEventListener('click', () => {
+    cookiesWrapper.classList.remove('visible')
+    // localStorage.setItem('cookie-consent', 'false')
+  })
+  acceptButton?.addEventListener('click', () => {
+    cookiesWrapper.classList.remove('visible')
+    // localStorage.setItem('cookie-consent', 'true')
+    consent = true
+    console.log('consent -> ', consent ? 'You have accepted cookies' : 'You have rejected cookies')
+  })
+  // Show the cookies wrapper after 2 seconds
+  // if the user has not already accepted or rejected cookies
 
   setTimeout(():void=> {
     cookiesWrapper.classList.add('visible')
