8294a8c7c574d31e99387c42680f7770791aa2c6
Robertron624 <robert.624@hotmail.com>
2025-04-25 16:25:23 -0500

    refactor: enhance cookie banner functionality by using a function to prevent code duplication

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,38 @@
-import './style.css'
-
+import "./style.css";
 
 function main() {
-  const savedConsent = localStorage.getItem('cookie-consent')
-  if (savedConsent === 'true') return
+  const savedConsent = localStorage.getItem("cookie-consent");
+  if (savedConsent === "true") return;
 
-  const cookiesWrapper = document.querySelector('.cookies-wrapper') as HTMLElement
-  const closeButton = document.querySelector('.cookies-wrapper .close')
-  const acceptButton = document.querySelector('.cookies-wrapper .accept')
-  if (!cookiesWrapper) return
+  const cookiesWrapper = document.querySelector(
+    ".cookies-wrapper"
+  ) as HTMLElement;
+  const closeButton = document.querySelector(".cookies-wrapper .close");
+  const acceptButton = document.querySelector(".cookies-wrapper .accept");
+  if (!cookiesWrapper) return;
 
   // We "render" the cookies banner after the DOM is loaded before making it visible
   // to avoid the flickering effect when the page loads
   // and the banner is not yet rendered
-  cookiesWrapper.classList.add('rendered')
+  cookiesWrapper.classList.add("rendered");
 
-  closeButton?.addEventListener('click', () => {
-    cookiesWrapper.classList.remove('visible')
-    localStorage.setItem('cookie-consent', 'false')
-  })
+  const closeCookieBanner = (consent: boolean) => {
+    cookiesWrapper.classList.remove("visible");
+    localStorage.setItem("cookie-consent", String(consent));
 
-  acceptButton?.addEventListener('click', () => {
-    cookiesWrapper.classList.remove('visible')
-    localStorage.setItem('cookie-consent', 'true')
-  })
+    // We wait for the animation to finish before removing the banner
+    setTimeout(() => {
+      cookiesWrapper.remove();
+    }, 500);
+  };
+
+  closeButton?.addEventListener("click", () => closeCookieBanner(false));
+  acceptButton?.addEventListener("click", () => closeCookieBanner(true));
 
   // Show the banner with a delay of 2 seconds using an animation
   setTimeout(() => {
-    cookiesWrapper.classList.add('visible')
-  }, 2000)
+    cookiesWrapper.classList.add("visible");
+  }, 2000);
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener("DOMContentLoaded", main);
